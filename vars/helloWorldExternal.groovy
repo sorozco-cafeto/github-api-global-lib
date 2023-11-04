5287545f02d756cf11ebc8e612bdb96f5d574ce6
Samith Orozco <141340701+sorozco-cafeto@users.noreply.github.com>
2023-11-03 19:04:01 -0500

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
-    loadLinuxScript(name: 'hello-world.sh')
-    sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+    loadLinuxScript(name: 'hello-world.bat')
+    "hello-world.bat ${config.name} ${config.dayOfWeek}"
 }
