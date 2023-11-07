524258fbbd2fc22f97abd743b9181768cdb39c46
Samith Orozco <141340701+sorozco-cafeto@users.noreply.github.com>
2023-11-06 21:39:19 -0500

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]) {
     loadLinuxScript(name: 'hello-world.bat') 
     echo "echo before running the bat file"
-    './hello-world.bat ${config.name} ${config.dayOfWeek} '
+    dir
+    ./hello-world.bat '${config.name} ' '${config.dayOfWeek} '
     echo "hello-world should now work"
 }
