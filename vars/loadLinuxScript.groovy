7f7e141c599fafc82b27807e62581d8ffe4b8d35
Samith Orozco <141340701+sorozco-cafeto@users.noreply.github.com>
2023-11-06 21:24:43 -0500

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) { 
   def scriptcontents = libraryResource "com/planetpope/scripts/windows/${config.name}"    
-  echo "echo from inside loadLinuxScript the variable : ${config.name} "
   writeFile file: "${config.name}", text: scriptcontents 
-  echo "echo from inside loadLinuxScript the second variable : ${scriptcontents}"
+  echo "echo from inside loadLinuxScript the variable config.name: ${config.name} "
+  echo "echo from inside loadLinuxScript the second variable scriptcontents: ${scriptcontents}"
   //attrib +x ./${config.name}
 } 
