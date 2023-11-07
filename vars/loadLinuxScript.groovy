0fcf79f8d642457bc088377c136dfef15678428a
Samith Orozco <141340701+sorozco-cafeto@users.noreply.github.com>
2023-11-06 20:53:03 -0500

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/planetpope/scripts/windows/${config.name}"    
-  writeFile file: "${config.name}", text: scriptcontents 
-  chmod a+x ${config.name}.bat
+  #def scriptcontents = libraryResource "com/planetpope/scripts/windows/${config.name}"    
+  #writeFile file: "${config.name}", text: scriptcontents 
+  #chmod a+x ${config.name}.bat
+  echo "echo from inside loadLinuxScript"
 } 
